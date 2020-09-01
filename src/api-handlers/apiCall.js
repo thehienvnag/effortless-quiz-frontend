46f5e1eba064350948b4b66458aeca7daeee77e9
thehienvnag <thehienvnag@gmail.com>
2020-09-01 08:33:23 +0700

    fix cors header

--- src/api-handlers/apiCall.js
+++ src/api-handlers/apiCall.js
@@ -7,11 +7,12 @@ import { mutationTypes } from "../store/mutations/userMutations";
 export const axiosInstance = axios.create({
   baseURL,
   responseType: "application/json",
+  withCredentials: false,
   headers: {
     "X-Requested-With": "XMLHttpRequest",
+    "Access-Control-Allow-Origin": "*"
   },
 });
-axiosInstance.defaults.headers["Access-Control-Allow-Origin"] = "*";
 
 export const fbAxiosInstance = axios.create({
   baseURL: fbBaseURL,
