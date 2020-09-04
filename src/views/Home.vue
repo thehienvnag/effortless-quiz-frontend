effbc21f1f0c7618b73121d49aab64463e06d21e
thehienvnag <thehienvnag@gmail.com>
2020-09-04 19:23:59 +0700

    Update home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,37 @@
 <template>
-  <div class="home" :style="{ backgroundColor: '#edf2f8', height: '82.8vh' }">
-    <div>
-      <div class="d-flex" :style="{ height: '500px', padding: '3em 3%' }">
-        <div
-          class=""
-          style="margin-top: 120px; width: 500px;"
-        >
-          <div class="text-center">
-            <span class="subheading">Best Page Of Taking Quiz</span>
-            <h1>The Wooden Of Knowledge</h1>
-            <p>
-              Where teacher easily give plenty of quiz without any efforts.
-              <br />Where student participate to enjoy taking quiz!
-            </p>
-          </div>
+  <div
+    class="home"
+    :style="{ backgroundColor: '#edf2f8', height: '82.8vh', padding: '2em' }"
+  >
+    <a-row type="flex" justify="space-around" align="middle">
+      <a-col :span="12">
+        <div :style="{ textAlign: 'center' }">
+          <span class="subheading">Best Page Of Taking Quiz</span>
+          <h1>The Wooden Of Knowledge</h1>
+          <p>
+            Where teacher easily give plenty of quiz without any efforts.
+            <br />Where student participate to enjoy taking quiz!
+          </p>
         </div>
+      </a-col>
+      <a-col :span="12">
         <img
           class=""
           src="../../public/cover_bg.svg"
           alt=""
-          style="height: 500px;"
+          :style="{ minWidth: '350px', maxWidth: '90%' }"
         />
-      </div>
-    </div>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import { Button, Row, Col } from "ant-design-vue";
+Vue.use(Button)
+  .use(Row)
+  .use(Col);
 export default {
   name: "Home",
 };
